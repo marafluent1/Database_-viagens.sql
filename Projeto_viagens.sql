19a744f231ecd993896746fc97e2283e0389c94a
marafluent1 <marafluent1@gmail.com>
2026-02-12 18:53:56 +0000

    criação tabela clientes
    adicionei tabela de id_clientes com nome, emial, telefone

--- Projeto_viagens.sql
+++ Projeto_viagens.sql
@@ -5,3 +5,40 @@ CREATE TABLE `DATABASE viagens` (
   `Viagens` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
+ Estrutura da tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nome`, `email`, `telefone`) VALUES
+(1, 'Ana Silva', 'ana@email.com', '912345678');
+
+
+Estrutura da tabela `destinos`
+--
+
+CREATE TABLE `destinos` (
+  `id_destino` int(11) NOT NULL,
+  `nome_destino` varchar(100) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `preco` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+
+Estrutura da tabela `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id_reserva` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_destino` int(11) NOT NULL,
+  `data_reserva` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
